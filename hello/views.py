3ffe73d08bd459694846951df70ac90138232eda
CalvinKrist <calvin.krist@yahoo.com>
2018-11-26 17:26:18 -0500

    django tests

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import Greeting
 
-# Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "myfile.html")
+    return HttpResponse('Hello from Python!')
 
 
 def db(request):
